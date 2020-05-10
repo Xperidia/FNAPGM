b17e9092eccd07c1a037619a79795c67235bebc2
VictorienXP <VictorienXP@Xperidia.com>
2020-05-10 23:08:21 +0200

    Update fnapgm_twilight.lua

--- lua/entities/fnapgm_twilight.lua
+++ lua/entities/fnapgm_twilight.lua
@@ -97,7 +97,11 @@ function ENT:Fall()
 		timer.Create("fnapgm_twilight_fall_sound_" .. self:EntIndex(), 0.5, 1,
 		function()
 
-			self:EmitSound(fall_sound, 140)
+			if IsValid(self) then
+
+				self:EmitSound(fall_sound, 140)
+
+			end
 
 		end)
 
