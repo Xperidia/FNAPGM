f0e923c42cc394b7f5758d0f348bc1e153ae55f0
VictorienXP <VictorienXP@Xperidia.com>
2020-05-21 14:25:15 +0200

    Update shared.lua

--- gamemodes/fnapgm/gamemode/shared.lua
+++ gamemodes/fnapgm/gamemode/shared.lua
@@ -15,7 +15,7 @@ GM.ShortName = "FNAPGM"
 GM.Author 	= "Xperidia"
 GM.Website 	= "steamcommunity.com/sharedfiles/filedetails/?id=542710334"
 
-GM.Version 	= 1.54
+GM.Version 	= 1.55
 
 if game.GetMap() == "fnap_cb" then
 	GM.FT = 2
